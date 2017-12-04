d4959f9b54351f76b5e304d53b0e45581921be90
cyfixus <seankfost@gmail.com>
2017-12-04 01:42:19 -0700

    gdd

--- main.cpp
+++ main.cpp
@@ -16,12 +16,13 @@ void newGame()
 
 int main()
 {
-  string welcome = "\nWelcome to Stuck.\nWhile flitting among the foliage,"
-                   " your carefree afternoon was interuppted as you clumsily"
-                   " fell through a patch of false earth.\nWhen you finally"
-                   " come to, you find yourself in a moist, dark, and smelly"
-                   " room. Strange sounds reverberate among the walls. There"
-                   " must be some way out of this dump.\n From the hole"
+  string welcome = "\nWelcome! You are Stuck.\nWhile flitting among the"
+                   " foliage, your carefree afternoon was interuppted as you" 
+                   " clumsily fell through a patch of false earth.\nWhen you"
+                   " finally come to, you find yourself in a moist, dark, and"
+                   " smelly room."
+                   "\n Strange sounds reverberate among the walls. There"
+                   " must be some way out of this dumps.\n From the hole"
                    " through which you fell, enough light has made it to the"
                    " depths to allow you take make out the four directions"
                    " within the room.\nWould you care to look around?\n1. Look"
